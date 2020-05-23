539eb0e00f1afa2db5f87f7cfefae24c6e9740ed
Ewa Czech <ewa.czech@hotmail.com>
2020-05-23 20:26:58 +0200

    add endpoints for put and delete

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -29,4 +29,51 @@ router.post('/', (req, res) => {
 		});
 });
 
+router.delete('/:id', (req, res) => {
+	const carId = req.params.id;
+	console.log(carId);
+	db('cars')
+		.where({ id: carId })
+		.del()
+		.then((cars) => {
+			if (cars > 0) {
+				res.status(200).json({ id: Number(req.params.id) });
+			} else {
+				res.status(404).json({
+					message: 'The car with the specified ID does not exist.',
+				});
+			}
+		})
+		.catch((error) => {
+			console.log(error);
+			res.status(500).json({
+				error: 'There was an error while saving a new car to the database.',
+			});
+		});
+});
+
+router.put('/:id', (req, res) => {
+	const carId = req.params.id;
+	const updatedCar = req.body;
+	console.log(carId);
+	db('cars')
+		.where({ id: carId })
+		.update(updatedCar)
+		.then((cars) => {
+			if (cars > 0) {
+				res.status(200).json({ id: Number(req.params.id) });
+			} else {
+				res.status(404).json({
+					message: 'The car with the specified ID does not exist.',
+				});
+			}
+		})
+		.catch((error) => {
+			console.log(error);
+			res.status(500).json({
+				error: 'Failed to edit a car.',
+			});
+		});
+});
+
 module.exports = router;
